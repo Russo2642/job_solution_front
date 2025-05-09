172b63d1fab0c9ad479466f363f01e4ccb302fba
Ruslan Zulufov <stucci@mail.ru>
2025-05-09 15:32:00 +0500

    Updates files

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -10,9 +10,11 @@ module.exports = {
   parser: '@typescript-eslint/parser',
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
+    'react-refresh/only-export-components': 'off',
+    'react-hooks/exhaustive-deps': 'warn',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'prefer-const': 'warn',
+    'no-empty': 'warn'
   },
 } 
